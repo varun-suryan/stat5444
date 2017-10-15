da2819e6fc0cde7b0357e75e995e8a86a783f0e7
Varun Suryan <varunsuryan9@gmail.com>
2017-10-15 16:57:44 -0400

    post-analysis-beta

--- prob_6.m
+++ prob_6.m
@@ -4,27 +4,26 @@ close all;
 
 original_p = [0.01:0.01:0.99];
 num_trials = 10000;
+num_samples = 5000;
 
-for N = [30 50 100];
-	total_data = zeros(size(original_p));
-	for trial = 1 : num_trials
-		observations = binornd(N, original_p);
-		p_hat = observations/N;
-		total_data = total_data + (abs(original_p - p_hat) <= 1.96 * sqrt( p_hat .* (1 - p_hat) / N ));
+data_n = [];
+for N = [30, 50, 100];
+	data = [];
+	for actual_p = original_p
+		actual_p
+		count = 0;
+		for trials = 1 : num_trials
+			observation = binornd(N, actual_p);
+			post_samples = sort(betarnd(0.5 + observation, N + 0.5 - observation, [1 num_samples]));
+			if actual_p > post_samples(0.025 * num_samples + 1) && actual_p < post_samples(0.975 * num_samples)
+				count = count + 1;
+			end
+		end
+		data = [data count];
 	end
-	
-	if N == 30
-		scatter(original_p, total_data, '+');
-	end
-	
-	if N ==50
-		scatter(original_p, total_data, 'o');
-	end
-	
-	if N == 100
-		scatter(original_p, total_data, 'd');
-	end
-	hold on;
+	data_n = [data_n ; data]
 end
 
 
+
+		
