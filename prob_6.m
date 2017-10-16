037e4e921522fdd016e4d87703baf0a392c8b537
Varun Suryan <varunsuryan9@gmail.com>
2017-10-16 03:46:57 -0400

    cauchy-simulation

--- prob_6.m
+++ prob_6.m
@@ -1,29 +1,9 @@
-clear all;
 clc;
+clear all;
 close all;
 
-original_p = [0.01:0.01:0.99];
-num_trials = 10000;
-num_samples = 5000;
 
-data_n = [];
-for N = [30, 50, 100];
-	data = [];
-	for actual_p = original_p
-		actual_p
-		count = 0;
-		for trials = 1 : num_trials
-			observation = binornd(N, actual_p);
-			post_samples = sort(betarnd(0.5 + observation, N + 0.5 - observation, [1 num_samples]));
-			if actual_p > post_samples(0.025 * num_samples + 1) && actual_p < post_samples(0.975 * num_samples)
-				count = count + 1;
-			end
-		end
-		data = [data count];
-	end
-	data_n = [data_n ; data]
-end
+tmp_samples = gamrnd(0.5, 2, [1 1000]);
+samples = normrnd(0, sqrt(1./tmp_samples));
 
-
-
-		
+hist(samples, 100)
